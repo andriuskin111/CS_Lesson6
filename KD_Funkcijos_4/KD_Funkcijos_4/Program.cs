6b3544e91b9aac91d0fd15f21f4e49d2a44340a5
andriuskin111 <andriuskin@gmail.com>
2020-01-14 20:43:07 +0200

    done

--- KD_Funkcijos_4/KD_Funkcijos_4/Program.cs
+++ KD_Funkcijos_4/KD_Funkcijos_4/Program.cs
@@ -10,6 +10,18 @@ namespace KD_Funkcijos_4
     {
         static void Main(string[] args)
         {
+            double mase = 75;
+            double ugis = 1.81;
+
+            Console.WriteLine(GrazintiKmi(mase, ugis));
+
+            Console.ReadLine();
+        }
+
+        static double GrazintiKmi(double mase, double ugis)
+        {
+            double kmi = mase / (ugis * ugis);
+            return kmi;
         }
     }
 }
