1194b08b60d3a4fb869e65b13285bb43d0bff4eb
andriuskin111 <andriuskin@gmail.com>
2020-01-14 20:37:30 +0200

    done

--- KD_Funkcijos_3/KD_Funkcijos_3/Program.cs
+++ KD_Funkcijos_3/KD_Funkcijos_3/Program.cs
@@ -10,6 +10,19 @@ namespace KD_Funkcijos_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(SuskaiciuotiSimbolius("Andrius"));
+
+            Console.ReadLine();
+        }
+
+        static string SuskaiciuotiSimbolius(string eilute)
+        {
+            string simboliai = "";
+            for (int i = 0; i < eilute.Length; i++)
+            {
+                simboliai += '#';
+            }
+            return simboliai;
         }
     }
 }
