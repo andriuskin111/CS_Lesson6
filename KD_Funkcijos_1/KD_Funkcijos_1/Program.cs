7b68da8af841b379ad9d7a4923ecf1aa0bd97e03
andriuskin111 <andriuskin@gmail.com>
2020-01-14 20:14:14 +0200

    Start

---
+++ KD_Funkcijos_1/KD_Funkcijos_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD_Funkcijos_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "Andrius";
+
+            IsvestiVarda(vardas);
+
+            Console.ReadLine();
+        }
+
+        static void IsvestiVarda(string vardas)
+        {
+            Console.WriteLine(vardas);
+        }
+    }
+}
