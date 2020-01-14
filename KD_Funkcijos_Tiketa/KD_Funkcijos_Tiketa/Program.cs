fc967e1b64ab9e8e7408a6e49af00c2eba235ebf
andriuskin111 <andriuskin@gmail.com>
2020-01-14 21:21:37 +0200

    start

---
+++ KD_Funkcijos_Tiketa/KD_Funkcijos_Tiketa/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD_Funkcijos_Tiketa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
