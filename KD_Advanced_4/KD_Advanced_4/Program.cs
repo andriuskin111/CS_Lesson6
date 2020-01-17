8ec13cf6d02256c1f3c132ba3550cc918905889e
andriuskin111 <andriuskin@gmail.com>
2020-01-17 16:22:44 +0200

    prideta rekursine funkcija

--- KD_Advanced_4/KD_Advanced_4/Program.cs
+++ KD_Advanced_4/KD_Advanced_4/Program.cs
@@ -10,6 +10,22 @@ namespace KD_Advanced_4
     {
         static void Main(string[] args)
         {
+            int skaicius = 5;
+            Console.WriteLine($"{skaicius}! = {parodytiFactoriala(1, skaicius)}");
+
+            Console.ReadLine();
+        }
+
+        static int parodytiFactoriala(int skaiciusNuo, int skaiciusIki)
+        {
+            int result = skaiciusNuo;
+
+            if(skaiciusNuo < skaiciusIki)
+            {
+                skaiciusNuo++;
+                return result *= parodytiFactoriala(skaiciusNuo, skaiciusIki);
+            }
+            return result;
         }
     }
 }
