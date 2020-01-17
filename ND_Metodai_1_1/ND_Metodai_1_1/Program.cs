76f6d4e36f7b1edf98fc820296d72f6e2c5fdd26
andriuskin111 <andriuskin@gmail.com>
2020-01-17 16:34:44 +0200

    vardas, pavarde metodas

--- ND_Metodai_1_1/ND_Metodai_1_1/Program.cs
+++ ND_Metodai_1_1/ND_Metodai_1_1/Program.cs
@@ -10,6 +10,19 @@ namespace ND_Metodai_1_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite varda:");
+            string vardas = Console.ReadLine();
+            Console.WriteLine("Iveskite pavarde:");
+            string pavarde = Console.ReadLine();
+            Console.WriteLine(sujungtiVardaPavarde(vardas, pavarde));
+
+            Console.ReadLine();
+        }
+
+        static string sujungtiVardaPavarde(string vardas, string pavarde)
+        {
+            string vardasPavarde = vardas + ", " + pavarde;
+            return vardasPavarde;
         }
     }
 }
