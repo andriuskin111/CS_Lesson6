b1608ed0f68a9b5f590b75b62f23752fc1194519
andriuskin111 <andriuskin@gmail.com>
2020-01-17 14:17:19 +0200

    prideta palindromo funkcija

--- KD_Advanced_1/KD_Advanced_1/Program.cs
+++ KD_Advanced_1/KD_Advanced_1/Program.cs
@@ -10,6 +10,49 @@ namespace KD_Advanced_1
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Iveskite zodi arba sakini: [exit] - iseiti is programos");
+                string fraze = Console.ReadLine();
+
+                if(fraze == "exit")
+                {
+                    Environment.Exit(0);
+                }
+
+                patikrintiArPalindromas(fraze);
+
+                Console.WriteLine(patikrintiArPalindromas(fraze));
+            }
+        }
+
+        static bool patikrintiArPalindromas(string fraze)
+        {
+            bool palindromas = false;
+            string[] separator = { " " };
+            string[] frazeArray = fraze.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            string frazeBeTarpu = string.Join("", frazeArray);
+            string frazeBeTarpuReversed = "";
+
+            for (int i = frazeBeTarpu.Length - 1; i >= 0 ; i--)
+            {
+                frazeBeTarpuReversed += string.Join("",frazeBeTarpu[i]);
+            }
+            
+            if(frazeBeTarpu == frazeBeTarpuReversed)
+            {
+                palindromas = true;
+            }
+            else
+            {
+                palindromas = false;
+            }
+
+            //Console.WriteLine(frazeBeTarpu);
+            //Console.WriteLine(frazeBeTarpuReversed);
+
+            
+            return palindromas;
         }
     }
 }
