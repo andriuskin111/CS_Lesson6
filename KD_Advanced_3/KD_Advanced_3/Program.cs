0c82839af9a233e90b7ead21566a1b0cf3961a63
andriuskin111 <andriuskin@gmail.com>
2020-01-17 15:47:52 +0200

    atskiru skaiciu skaiciavimas

--- KD_Advanced_3/KD_Advanced_3/Program.cs
+++ KD_Advanced_3/KD_Advanced_3/Program.cs
@@ -10,6 +10,43 @@ namespace KD_Advanced_3
     {
         static void Main(string[] args)
         {
+            int skaiciu = 1234567;
+
+            suskaiciuotiAtskiruSkaitmenuSuma(skaiciu);
+
+            Console.ReadLine();
+        }
+
+        static void suskaiciuotiAtskiruSkaitmenuSuma(int skaicius)
+        {
+            string skaiciusString = skaicius.ToString();
+            int[] skaiciusArray = new int[skaiciusString.Length];
+            string[] skaiciusStringArray = new string[skaiciusString.Length];
+
+            for (int i = 0; i < skaiciusString.Length; i++)
+            {
+                skaiciusStringArray[i] = Convert.ToString(skaiciusString[i]);
+            }
+
+            for (int i = 0; i < skaiciusArray.Length; i++)
+            {
+                skaiciusArray[i] = Convert.ToInt32(skaiciusStringArray[i]);
+                Console.Write($"{skaiciusArray[i]} ");
+
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < skaiciusArray.Length; i++)
+            {
+                int suma = 0;
+
+                for (int k = 0; k < skaiciusArray[i]; k++)
+                {
+                    suma += k;                   
+                }
+                Console.Write($"{suma} ");
+            }
         }
     }
 }
