b84a7e5efa121a186be342a0aa99ffcc8391f58d
andriuskin111 <andriuskin@gmail.com>
2020-01-17 13:23:16 +0200

    pridetos perkrautos funkcijos

--- KD_Funkcijos_2_4/KD_Funkcijos_2_4/Program.cs
+++ KD_Funkcijos_2_4/KD_Funkcijos_2_4/Program.cs
@@ -10,6 +10,37 @@ namespace KD_Funkcijos_2_4
     {
         static void Main(string[] args)
         {
+            int pirmasSkaicius = 10;
+            int antrasSkaicius = 20;
+            bool rodytiTarpineSuma = true;
+
+            isvestiSuma(pirmasSkaicius, antrasSkaicius);
+            isvestiSuma(pirmasSkaicius, antrasSkaicius, rodytiTarpineSuma);
+
+            Console.ReadLine();
+        }
+
+        static void isvestiSuma(int nuo, int iki)
+        {
+            int suma = 0;
+            for (int i = nuo; i <= iki; i++)
+            {
+                suma += i;
+            }
+
+            Console.WriteLine($"Skaiciu suma nuo {nuo} iki {iki} lygi {suma}");
+        }
+        static void isvestiSuma(int nuo, int iki, bool rodytiTarpineSuma)
+        {
+            int sum = 0;
+
+            Console.WriteLine($"Skaiciu nuo {nuo} iki {iki} tarpine suma:");
+            for (int i = nuo; i <= iki; i++)
+            {
+                Console.Write(sum);
+                sum += i;
+                Console.WriteLine($" + {i} = {sum}");
+            }
         }
     }
 }
