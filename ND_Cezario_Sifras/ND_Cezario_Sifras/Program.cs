0a74e8aa9cbd30ac61838e082cec3ac14845cfd8
andriuskin111 <andriuskin@gmail.com>
2020-01-21 01:50:27 +0200

    pridetas cezario sifras

--- ND_Cezario_Sifras/ND_Cezario_Sifras/Program.cs
+++ ND_Cezario_Sifras/ND_Cezario_Sifras/Program.cs
@@ -10,6 +10,28 @@ namespace ND_Cezario_Sifras
     {
         static void Main(string[] args)
         {
+            string sakinys = "Kosmonautas isskrido i menuli";
+            string uzsifruotasSakinys = "";
+            string desifruotasSakinys = "";
+            string slaptazodis = "4";
+
+            for (int i = 0; i < sakinys.Length - 1; i++)
+            {
+                int simbolis = (int)sakinys[i];
+                simbolis += int.Parse(slaptazodis);
+                uzsifruotasSakinys += (char)simbolis;
+            }
+
+            for (int i = 0; i < sakinys.Length - 1; i++)
+            {
+                int simbolis = (int)uzsifruotasSakinys[i];
+                simbolis -= int.Parse(slaptazodis);
+                desifruotasSakinys += (char)simbolis;
+            }
+
+            Console.WriteLine($"Sakinys: {sakinys}\nUzsifroutas: {uzsifruotasSakinys}\nDesifruotas: {desifruotasSakinys}");
+
+            Console.ReadLine();
         }
     }
 }
