f0579723c6ce570ed44490824bda9ed1b2b3a58c
andriuskin111 <andriuskin@gmail.com>
2020-01-21 00:49:32 +0200

    sukurtas projektas

---
+++ ND_Cezario_Sifras/ND_Cezario_Sifras/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ND_Cezario_Sifras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
